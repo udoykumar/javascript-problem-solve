02c1d3545bca501d0540e5975b690d21b56175cd
udoykumar <udoypal.kumar2002@gmail.com>
2026-02-04 21:43:28 +0600

    not problem solve simple practice

--- array-map.js
+++ array-map.js
@@ -1,13 +1,20 @@
 const numbers = [4, 8, 2, 3, 5];
-const double = [];
+// const double = [];
 
 // for (const num of numbers) {
 //   const result = num * 2;
 //   double.push(result);
 // }
 
-numbers.map((num) => {
+const doubleIt = numbers.map((num) => {
   const result = num * 2;
-  double.push(result);
+  return result;
 });
-console.log(double);
+const double = numbers.map((x) => x * 2);
+console.log("single line map", double);
+console.log("map", doubleIt);
+
+const friend = ["zaved", "pavel", "nobel", "kodbel"];
+// const nameLength = friend.map((name) => name.length);
+const nameLength = friend.map((name) => name[1].toUpperCase());
+console.log("name length", nameLength);
